8151ee0402e3475e6c40fd3301b5a84ec4d78e7c
Luishvdiaz <luisspartan12@outlook.com>
2022-11-11 17:16:12 -0600

    Update contact email

--- src/views/Footer/Footer.tsx
+++ src/views/Footer/Footer.tsx
@@ -8,7 +8,7 @@ const Footer: React.FC = () => {
       <img src="/images/logo.PNG" alt="Logo" className='home__image-container__logo' />
       <div className='footer__contact'>
         <b>Contacto</b>
-        <a href="mailto:conctacto@altosinthehouse.mx">conctacto@altosinthehouse.mx</a>
+        <a href="mailto:contacto@altosinthehouse.mx">contacto@altosinthehouse.mx</a>
         <a href="tel:+523481051510" target="_blank" rel="noreferrer">Teléfono: 348-59-39-432</a>
       </div>
     </div>
